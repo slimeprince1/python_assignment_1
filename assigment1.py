fa46919aab33986e8f61d8790bc310c21e82f080
Dennis Bamfo <dennisbamfo@meltwater.org>
2022-10-11 21:31:42 +0100

    First python assignment

---
+++ assigment1.py
@@ -0,0 +1,5 @@
+
+first_number = int(input("Enter first number     "))
+second_number = int(input("Enter second number   "))
+total_number = first_number + second_number
+print("Total number is ", (total_number))
---
+++ intro.py
@@ -0,0 +1,1 @@
+print("Hello world, my name is Dennis. I am from Ghana. I am 2000 years old . I love to swim and code plus i want solved the terrorism problem in Kenya")
